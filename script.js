ededa72ec3c951ea7ec99dfa34f1571724e3ed43
dee-diaz <diazthediablo@yahoo.com>
2025-06-23 16:55:23 -0300

    Add create grid functionality

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector(".container");
+const DEFAULT_NUM_OF_SQUARES = 16;
 
-function createDiv() {
-  const div = document.createElement("div");
+function createGrid(numOfSquares) {
+  const totalSquares = numOfSquares ** 2;
+
+  for (let i = 0; i < totalSquares; i++) {
+    const square = document.createElement("div");
+    container.appendChild(square);
+  }
 }
+
+
+createGrid(DEFAULT_NUM_OF_SQUARES);
